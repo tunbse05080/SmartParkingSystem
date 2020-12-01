2460a51f8fd5c586c956d2b3ccdb02acc31b7994
NguyenMinhCuong006 <71684525+NguyenMinhCuong006@users.noreply.github.com>
2020-12-01 14:17:12 +0700

    Update ManageCardController

--- SmartParkingApplication/Controllers/ManageCardController.cs
+++ SmartParkingApplication/Controllers/ManageCardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmartParkingApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,24 @@ namespace SmartParkingApplication.Controllers
     public class ManageCardController : Controller
     {
         // GET: ManageCard
+        private SmartParkingsEntities db = new SmartParkingsEntities();
+        // GET: ManageCard
         public ActionResult Index()
         {
             return View();
+        }
+        public JsonResult LoadData(int pageCard, String nameC, int pageSizeCard = 3)
+        {
+
+            var CardNumber = from c in db.Cards select c;
+            if (!string.IsNullOrEmpty(nameC))
+            {
+                CardNumber = CardNumber.Where(x => x.CardNumber.Contains(nameC));
+            }
+            var totalRow = CardNumber.Count();
+            CardNumber = CardNumber.OrderByDescending(x => x.CardID).Skip((pageCard - 1) * pageSizeCard).Take(pageSizeCard);
+
+            return Json(new { dataCard = CardNumber, total = totalRow }, JsonRequestBehavior.AllowGet);
         }
     }
 }
