7746bc8ab27206f7ae510d0f460504f9d043a413
NguyenMinhCuong006 <71684525+NguyenMinhCuong006@users.noreply.github.com>
2020-12-02 10:54:02 +0700

    LoadDateTicketNow

--- SmartParkingApplication/Content/dist/js/CRUD_Ticket.js
+++ SmartParkingApplication/Content/dist/js/CRUD_Ticket.js
@@ -80,3 +80,22 @@ function clearTextBoxTicket() {
     $('#btnUpdate').hide();
 
 }
+
+function loadDateTicketNow() {
+    // body...
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+
+    today = mm + '/' + dd + '/' + yyyy;
+    today = today + " 12:00:00AM";
+    return today;
+}
