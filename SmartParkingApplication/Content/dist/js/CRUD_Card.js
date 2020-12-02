3e2e5de8b05bb12a3865adb4a816ba11b6d773cd
Quan11235 <71583977+Quan11235@users.noreply.github.com>
2020-12-02 11:07:23 +0700

    valid add + update

--- SmartParkingApplication/Content/dist/js/CRUD_Card.js
+++ SmartParkingApplication/Content/dist/js/CRUD_Card.js
@@ -173,7 +173,7 @@ function getCardByID(CardID) {
 function validate{
     var isValid = true;
     var rfidCard = new RegExp('^[0-9]{10,}$');
-    if ($('#CardNumber').val().trim() == "" || $('#CardNumber').val().trim().length < 10) {
+    if ($('#CardNumber').val().trim() == "" || !rfidCard.test($('#CardNumber').val().trim())) {
         $('#CardNumber').prop("title", "Số thẻ trống hoặc sai định dạng(>9 số).");
         $('#CardNumber').css('border-color', 'Red');
         isValid = false;
