065cf2650d83d766e2f26d15cf093a34d19bad48
tunbse05080 <71684528+tunbse05080@users.noreply.github.com>
2020-12-06 23:39:23 +0700

    Update UI SettingPrice

---
+++ SmartParkingApplication/Controllers/SettingPriceController.cs
@@ -0,0 +1,19 @@
+﻿using SmartParkingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartParkingApplication.Controllers
+{
+    public class SettingPriceController : Controller
+    {
+        // GET: SettingPrice
+        private SmartParkingsEntities db = new SmartParkingsEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
