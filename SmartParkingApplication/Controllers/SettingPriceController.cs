4128b0ddf50423e4318c91ff44df69a5d086131b
DuongXuanThang <71684568+DuongXuanThang@users.noreply.github.com>
2020-12-07 19:32:49 +0700

    SettingPriceloadData v1

--- SmartParkingApplication/Controllers/SettingPriceController.cs
+++ SmartParkingApplication/Controllers/SettingPriceController.cs
@@ -15,5 +15,24 @@ namespace SmartParkingApplication.Controllers
         {
             return View();
         }
+        public JsonResult LoadDataPrice(int pagepr, String namepr, int pageSizepr = 5)
+        {
+            var set = from s in db.Prices select new { s.PriceID, s.TypeOfvehicle, s.DayPrice, s.MonthPrice, s.FirstBlock, s.NextBlock };
+
+
+            List<Object> list = new List<object>();
+            foreach (var item in set)
+            {
+
+
+                var s = new { PriceID = item.PriceID, TypeOfvehicle = item.TypeOfvehicle, MonthPrice = item.MonthPrice, FirstBlock = item.FirstBlock, NextBlock = item.NextBlock };
+                list.Add(s);
+            }
+
+            var totalRowpr = list.Count();
+            var result = list.Skip((pagepr - 1) * pageSizepr).Take(pageSizepr);
+
+            return Json(new { datapr = result, total = totalRowpr }, JsonRequestBehavior.AllowGet);
+        }
     }
 }
