2b122f81d5dd54283f66dd582338700b6bc1925f
DuongXuanThang <71684568+DuongXuanThang@users.noreply.github.com>
2020-12-07 11:28:25 +0700

    settingPrice

---
+++ SmartParkingApplication/Content/dist/js/SettingTicket.js
@@ -0,0 +1,45 @@
+﻿var pageConfigpp = 1;
+
+function loadDataParkingPlace(changsizepp) {
+    var namepp = $('#txtNameSearchpp').val();
+    $.ajax({
+        url: "/ManagePPlace/loadDataParkingPlace",
+        type: "GET",
+        contentType: "application/json;charset=utf-8",
+        data: {
+            namepp: namepp,
+            pagepp: pageConfigpp,
+            pageSizepp: 5,
+
+        },
+        dataType: "json",
+        success: function (result) {
+            var data = result.datapp;
+            var html = '';
+            $.each(data, function (key, item) {
+                html += '<tr>';
+                html += '<td>' + item.NameOfParking + '</td>';
+                html += '<td>' + item.Location + '</td>';
+                html += '<td>' + item.NumberOfCar + '</td>';
+                html += '<td>' + item.NumberOfMotoBike + '</td>';
+                html += '<td>' + item.NumberCarBlank + '</td>';
+                html += '<td>' + item.NumberMotoBikeBlank + '</td>';
+                html += '<td><button class="btn btn-success" onclick="return getPPByID(' + item.ParkingPlaceID + ')" > Sửa</button></td>';
+
+
+                html += '</tr>';
+            });
+            $('#tbodypp').html(html);
+            pagingpp(result.total, function () {
+                loadDataParkingPlace();
+            }, changsizepp);
+        },
+        error: function (errormessage) {
+            if (timeFrom == null && timeTo == null) {
+                alert("Làm ơn nhập thời gian vào và ra!");
+            } else {
+                alert(errormessage.responseText);
+            }
+        }
+    });
+}
--- SmartParkingApplication/Controllers/SettingController.cs
+++ SmartParkingApplication/Controllers/SettingController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmartParkingApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,30 @@ namespace SmartParkingApplication.Controllers
 {
     public class SettingController : Controller
     {
+        private SmartParkingsEntities db = new SmartParkingsEntities();
         // GET: Setting
         public ActionResult Index()
         {
             return View();
+        }
+        public JsonResult LoadDataPrice(int page, String name, int pageSize = 5  )
+        {
+            var set = from s in db.Prices select new { s.PriceID,s.TypeOfvehicle,s.DayPrice,s.MonthPrice,s.FirstBlock,s.NextBlock};
+           
+
+            List<Object> list = new List<object>();
+            foreach (var item in set)
+            {
+               
+               
+                var tr = new { PriceID = item.PriceID, TypeOfvehicle = item.TypeOfvehicle, MonthPrice = item.MonthPrice, FirstBlock = item.FirstBlock,NextBlock = item.NextBlock };
+                list.Add(tr);
+            }
+
+            var totalRow = list.Count();
+            var result = list.Skip((page - 1) * pageSize).Take(pageSize);
+
+            return Json(new { data = result, total = totalRow }, JsonRequestBehavior.AllowGet);
         }
     }
 }
