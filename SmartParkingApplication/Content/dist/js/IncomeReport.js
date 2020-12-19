9e095f466807d079596b0150582333eacbeba490
tunbse05080 <71684528+tunbse05080@users.noreply.github.com>
2020-12-19 18:48:39 +0700

    Update IncomeReport

---
+++ SmartParkingApplication/Content/dist/js/IncomeReport.js
@@ -0,0 +1,53 @@
+﻿$(document).ready(function () {
+    loadChartIncome();
+    ComboboxStaffName();
+});
+
+//Load Chart Income
+function loadChartIncome() {
+    var idParking = $('#cbNameParkingPlace').val();
+    var idTypeOfTicket = $('#cbNameStaff').val();
+    if (!idParking) {
+        idParking = 1;
+    }
+    if (!idTypeOfTicket) {
+        idTypeOfTicket = 0;
+    }
+    if ($('#cbTypeOfTicket').val() == 0) {
+        $('#dvParking').hide();
+    } else {
+        $('#dvParking').show();
+    }
+    $.ajax({
+        url: "/StatisticReport/LoadDataIncome",
+        type: "POST",
+        contents: "application/json",
+        data: { idParking: idParking, idTypeOfTicket: idTypeOfTicket },
+        dataType: "json",
+        success: function (result) {
+            ChartIncome(result);
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+function ComboboxStaffName() {
+    $.ajax({
+        url: "/StatisticReport/ComboboxStaffName",
+        type: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (result) {
+            var html = '';
+            $.each(result, function (key, item) {
+                html += '<option value=' + item.UserID + '>' + item.UserName + '</option>';
+            });
+            $('#cbNameStaff').html(html);
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
--- SmartParkingApplication/Controllers/StatisticReportController.cs
+++ SmartParkingApplication/Controllers/StatisticReportController.cs
@@ -7,12 +7,22 @@ using System.Web.Mvc;
 
 namespace SmartParkingApplication.Controllers
 {
-    public class ManageStatisticController : Controller
+    public class StatisticReportController : Controller
     {
         // GET: ManageStatistic
         private SmartParkingsEntities db = new SmartParkingsEntities();
 
         public ActionResult IncomeStatistic()
+        {
+            return View();
+        }
+
+        public ActionResult DensityStatistic()
+        {
+            return View();
+        }
+
+        public ActionResult IncomeReport()
         {
             return View();
         }
@@ -24,7 +34,7 @@ namespace SmartParkingApplication.Controllers
             List<double> listIncomeCar = new List<double>();
             for (int i = 0; i < 12; i++)
             {
-                
+
                 if (idTypeOfTicket == 1)
                 {
                     //get income dataMoto of DailyTicket ( most nearly 12 months )
@@ -52,8 +62,8 @@ namespace SmartParkingApplication.Controllers
 
                     //get income dataCar of MonthlyTicket ( most nearly 12 months )
                     var dataCar = (from mi in db.MonthlyIncomeStatements
-                                    where (mi.MonthlyTicket.RegisDate.Value.Month == DateTime.Now.Month - i) && (mi.MonthlyTicket.TypeOfVehicle == 1)
-                                    select new { mi.TotalPrice }).ToList();
+                                   where (mi.MonthlyTicket.RegisDate.Value.Month == DateTime.Now.Month - i) && (mi.MonthlyTicket.TypeOfVehicle == 1)
+                                   select new { mi.TotalPrice }).ToList();
                     var sumCar = dataCar.Select(s => s.TotalPrice).Sum();
                     listIncomeCar.Add((double)sumCar);
                 }
@@ -63,13 +73,8 @@ namespace SmartParkingApplication.Controllers
             return Json(new { listIncomeMoto, listIncomeCar }, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult DensityStatistic()
-        {
-            return View();
-        }
-
         //Load Chart CarDensity
-        public JsonResult loadChartCarDensity(int idParking)
+        public JsonResult LoadChartCarDensity(int idParking)
         {
             List<double> listMotoDestiny = new List<double>();
             List<double> listCarDestiny = new List<double>();
@@ -92,13 +97,31 @@ namespace SmartParkingApplication.Controllers
             return Json(new { listMotoDestiny, listCarDestiny }, JsonRequestBehavior.AllowGet);
         }
 
+        //Load Chart CarDensity
+        public JsonResult LoadDataIncomeReport(int idParking,int idStaff, DateTime date, int workingShift)
+        {
+            List<Object> listMotoDestiny = new List<Object>();
+            //
+            var result = "";
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+
         //combobox Gender
         public JsonResult ComboboxNameParking()
         {
             var list = (from p in db.ParkingPlaces
                         select new { p.ParkingPlaceID, p.NameOfParking }).ToList();
-
             return Json(list, JsonRequestBehavior.AllowGet);
         }
+
+        //public JsonResult ComboboxStaffName()
+        //{
+        //    var list = (from p in db.Users
+        //                where p.AccountID != null && p.Account.RoleID == 2
+        //                select new { p.UserID, p.Account.UserName }).ToList();
+
+        //    return Json(list, JsonRequestBehavior.AllowGet);
+        //}
     }
 }
