4307f4f13b83d7aae62508716c247c6228805915
Quan11235 <71583977+Quan11235@users.noreply.github.com>
2020-12-20 15:17:10 +0700

    update validate 20/12

--- SmartParkingApplication/Content/dist/js/Account.js
+++ SmartParkingApplication/Content/dist/js/Account.js
@@ -105,10 +105,10 @@ function LoadDataAccount() {
 
 //Add Data Function
 function Add() {
-    //var res = validate();
-    //if (res == false) {
-    //    return false;
-    //}
+    var res = validate();
+    if (res == false) {
+        return false;
+    }
     var empObj = {
         Name: $('#FullName').val(),
         DateOfBirth: $('#DateOfBirth').val(),
@@ -176,4 +176,103 @@ function Update() {
             alert(errormessage.responseText);
         }
     });
+}
+
+//Valdidation using jquery
+function validateAccEdit() {
+    var isValid = true;
+    var email = new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$');
+    var pwd = new RegExp('(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})');
+    var idcard = new RegExp('^[0-9]{9,}$');
+    var phone = new RegExp('^(09|03|07|08|05){1}([0-9]{8})$');
+    if ($.trim($('#UserName').val()) == "" || $.trim($('#UserName').val()).length < 4) {
+        $('#UserName').prop("title", "Tên tài khoản > 4 ký tự.");
+        $('#UserName').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#UserName').prop("title", "");
+        $('#UserName').css('border-color', 'lightgrey');
+    }
+    if ($.trim($('#PassWord').val()) == "" || !pwd.test($.trim($('#PassWord').val()))) {
+        $('#PassWord').prop("title", "Mật khấu >= 6 ký tự (chữ hoa, thường, số, ký tự đặc biệt.)");
+        $('#PassWord').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#PassWord').prop("title", "");
+        $('#PassWord').css('border-color', 'lightgrey');
+    }
+    if ($.trim($('#FullName').val()) == "" || $.trim($('#FullName').val()).length < 4) {
+        $('#FullName').prop("title", "Tên đầy đủ > 4 ký tự.");
+        $('#FullName').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#FullName').prop("title", "");
+        $('#FullName').css('border-color', 'lightgrey');
+    }
+    if ($.trim($('#DateOfBirth').val()) == "") {
+        $('#DateOfBirth').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#DateOfBirth').css('border-color', 'lightgrey');
+    }
+    if ($.trim($('#Gender').val()) == "") {
+        $('#Gender').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#Gender').css('border-color', 'lightgrey');
+    }
+    if ($.trim($('#Address').val()) == "") {
+        $('#Address').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#Address').css('border-color', 'lightgrey');
+    }
+    if ($.trim($('#PhoneNumber').val()) == "" || !phone.test($.trim($('#PhoneNumber').val()))) {
+        $('#PhoneNumber').prop("title", "Số điện thoại trống hoặc định dạng sai.");
+        $('#PhoneNumber').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#PhoneNumber').prop("title", "");
+        $('#PhoneNumber').css('border-color', 'lightgrey');
+    }
+    if ($.trim($('#Email').val()) == "" || !email.test($.trim($('#Email').val()))) {
+        $('#Email').prop("title", "Email trống hoặc không đúng định dạng.");
+        $('#Email').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#Email').prop("title", "");
+        $('#Email').css('border-color', 'lightgrey');
+    }
+    if ($.trim($('#IdentityCard').val()) == "" || !idcard.test($.trim($('#IdentityCard').val()))) {
+        $('#IdentityCard').prop("title", "CMND/CCCD trống hoặc sai định dạng.");
+        $('#IdentityCard').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#IdentityCard').prop("title", "")
+        $('#IdentityCard').css('border-color', 'lightgrey');
+    }
+    if ($.trim($('#ParkingPlace').val()) == "") {
+        $('#ParkingPlace').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#ParkingPlace').css('border-color', 'lightgrey');
+    }
+    if ($.trim($('#RoleName').val()) == "") {
+        $('#RoleName').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#RoleName').css('border-color', 'lightgrey');
+    }
+    return isValid;
 }
--- SmartParkingApplication/Content/dist/js/CRUD_User.js
+++ SmartParkingApplication/Content/dist/js/CRUD_User.js
@@ -266,7 +266,7 @@ function loadData() {
 
 //function for updating employee's record
 function Update() {
-    //var res = validate();
+    //var res = validateUserEdit();
     //if (res == false) {
     //    return false;
     //}
@@ -344,100 +344,66 @@ function clearTextBox() {
 }
 
 //Valdidation using jquery
-function validate() {
+function validateUserEdit() {
     var isValid = true;
     var email = new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$');
-    var pwd = new RegExp('(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})');
     var idcard = new RegExp('^[0-9]{9,}$');
     var phone = new RegExp('^(09|03|07|08|05){1}([0-9]{8})$');
-    if ($.trim($('#UserName').val()) == "" || $.trim($('#UserName').val()).length < 4) {
-        $('#UserName').prop("title", "Tên tài khoản > 4 ký tự.");
-        $('#UserName').css('border-color', 'Red');
+    if ($.trim($('#FullNameEdit').val()) == "" || $.trim($('#FullNameEdit').val()).length < 4) {
+        $('#FullNameEdit').prop("title", "Tên đầy đủ > 4 ký tự.");
+        $('#FullNameEdit').css('border-color', 'Red');
         isValid = false;
     }
     else {
-        $('#UserName').prop("title", "");
-        $('#UserName').css('border-color', 'lightgrey');
+        $('#FullNameEdit').prop("title", "");
+        $('#FullNameEdit').css('border-color', 'lightgrey');
     }
-    if ($.trim($('#PassWord').val()) == "" || !pwd.test($.trim($('#PassWord').val()))) {
-        $('#PassWord').prop("title", "Mật khấu >= 6 ký tự (chữ hoa, thường, số, ký tự đặc biệt.)");
-        $('#PassWord').css('border-color', 'Red');
+    if ($('#DateOfBirthEdit').val().length == 0) {
+        $('#DateOfBirthEdit').prop("title", "Chưa chọn ngày!");
+        $('#DateOfBirthEdit').css('border-color', 'Red');
+        isValid = false;
+    } else {
+        if (new Date($('#DateOfBirthEdit').val()) < new Date()) {
+            $('#DateOfBirthEdit').prop("title", "Nhỏ hơn ngày hiện tại!");
+            $('#DateOfBirthEdit').css('border-color', 'Red');
+            isValid = false;
+        } else {
+            $('#DateOfBirthEdit').css('border-color', 'lightgrey');
+        }
+    }
+    if ($.trim($('#AddressEdit').val()) == "") {
+        $('#AddressEdit').css('border-color', 'Red');
         isValid = false;
     }
     else {
-        $('#PassWord').prop("title", "");
-        $('#PassWord').css('border-color', 'lightgrey');
+        $('#AddressEdit').css('border-color', 'lightgrey');
     }
-    if ($.trim($('#FullName').val()) == "" || $.trim($('#FullName').val()).length < 4) {
-        $('#FullName').prop("title", "Tên đầy đủ > 4 ký tự.");
-        $('#FullName').css('border-color', 'Red');
+    if ($.trim($('#PhoneNumberEdit').val()) == "" || !phone.test($.trim($('#PhoneNumberEdit').val()))) {
+        $('#PhoneNumberEdit').prop("title", "Số điện thoại trống hoặc định dạng sai.");
+        $('#PhoneNumberEdit').css('border-color', 'Red');
         isValid = false;
     }
     else {
-        $('#FullName').prop("title", "");
-        $('#FullName').css('border-color', 'lightgrey');
+        $('#PhoneNumberEdit').prop("title", "");
+        $('#PhoneNumberEdit').css('border-color', 'lightgrey');
     }
-    if ($.trim($('#DateOfBirth').val()) == "") {
-        $('#DateOfBirth').css('border-color', 'Red');
+    if ($.trim($('#EmailEdit').val()) == "" || !email.test($.trim($('#EmailEdit').val()))) {
+        $('#EmailEdit').prop("title", "Email trống hoặc không đúng định dạng.");
+        $('#EmailEdit').css('border-color', 'Red');
         isValid = false;
     }
     else {
-        $('#DateOfBirth').css('border-color', 'lightgrey');
+        $('#EmailEdit').prop("title", "");
+        $('#EmailEdit').css('border-color', 'lightgrey');
     }
-    if ($.trim($('#Gender').val()) == "") {
-        $('#Gender').css('border-color', 'Red');
+    if ($.trim($('#IdentityCardEdit').val()) == "" || !idcard.test($.trim($('#IdentityCardEdit').val()))) {
+        $('#IdentityCardEdit').prop("title", "CMND/CCCD trống hoặc sai định dạng.");
+        $('#IdentityCardEdit').css('border-color', 'Red');
         isValid = false;
     }
     else {
-        $('#Gender').css('border-color', 'lightgrey');
-    }
-    if ($.trim($('#Address').val()) == "") {
-        $('#Address').css('border-color', 'Red');
-        isValid = false;
-    }
-    else {
-        $('#Address').css('border-color', 'lightgrey');
-    }
-    if ($.trim($('#PhoneNumber').val()) == "" || !phone.test($.trim($('#PhoneNumber').val()))) {
-        $('#PhoneNumber').prop("title", "Số điện thoại trống hoặc định dạng sai.");
-        $('#PhoneNumber').css('border-color', 'Red');
-        isValid = false;
-    }
-    else {
-        $('#PhoneNumber').prop("title", "");
-        $('#PhoneNumber').css('border-color', 'lightgrey');
-    }
-    if ($.trim($('#Email').val()) == "" || !email.test($.trim($('#Email').val()))) {
-        $('#Email').prop("title", "Email trống hoặc không đúng định dạng.");
-        $('#Email').css('border-color', 'Red');
-        isValid = false;
-    }
-    else {
-        $('#Email').prop("title", "");
-        $('#Email').css('border-color', 'lightgrey');
-    }
-    if ($.trim($('#IdentityCard').val()) == "" || !idcard.test($.trim($('#IdentityCard').val()))) {
-        $('#IdentityCard').prop("title", "CMND/CCCD trống hoặc sai định dạng.");
-        $('#IdentityCard').css('border-color', 'Red');
-        isValid = false;
-    }
-    else {
-        $('#IdentityCard').prop("title", "")
-        $('#IdentityCard').css('border-color', 'lightgrey');
-    }
-    if ($.trim($('#ParkingPlace').val()) == "") {
-        $('#ParkingPlace').css('border-color', 'Red');
-        isValid = false;
-    }
-    else {
-        $('#ParkingPlace').css('border-color', 'lightgrey');
-    }
-    if ($.trim($('#RoleName').val()) == "") {
-        $('#RoleName').css('border-color', 'Red');
-        isValid = false;
-    }
-    else {
-        $('#RoleName').css('border-color', 'lightgrey');
+        $('#IdentityCardEdit').prop("title", "")
+        $('#IdentityCardEdit').css('border-color', 'lightgrey');
     }
     return isValid;
 }
