92c56f206324e949eb5fefeb0a84451943fdf8c5
Quan11235 <71583977+Quan11235@users.noreply.github.com>
2020-12-29 20:45:40 +0700

    Update status parking

--- SmartParkingApplication/Content/dist/js/CRUDStatusParking.js
+++ SmartParkingApplication/Content/dist/js/CRUDStatusParking.js
@@ -1,62 +1,58 @@
 ﻿//load page
-$(document).ready(function () {
-    loadDataStatusParking();
-    //setInterval(function () {
-    //    table.ajax.reload();
-    //}, 30000);
-});
+//$(document).ready(function () {
+//    loadDataStatusParking();
+//    //setInterval(function () {
+//    //    table.ajax.reload();
+//    //}, 30000);
+//});
 
 //var table;
 
 //Load Data function
 function loadDataStatusParking() {
-    //var timeTo = $('#txtTimeToSPP').val();
-    //var timeFrom = $('#txtTimeFromSPP').val();
-    $('#cbxListParking').on('change', function () {
-        var ParkingPlaceID = $('#cbxListParking').val();
-        
-        $.ajax({
-            url: "/ManagePPlace/LoadDataStatusPP",
-            type: "GET",
-            data: { ParkingPlaceID: ParkingPlaceID },
-            contentType: "application/json;charset=utf-8",
-            //data: {
-            //    timeFrom: timeFrom,
-            //    timeTo: timeTo
-            //},
-            dataType: "json",
-            success: function (result) {
-                var data = result.dataSSP;
-                var html = '';
-                $.each(data, function (key, item) {
-                    html += '<tr>';
-                    html += '<td>' + item.LicensePlates + '</td>';
-                    html += '<td>' + item.TimeIn + '</td>';
-                   
-                    html += '<td>' + item.TypeOfTicket + '</td>';
-                    html += '<td>' + item.CardNumber + '</td>';
-                    html += '<td>' + item.TypeOfVerhicleTran + '</td>';
-                    html += '<td><button class="btn btn-primary" onclick = "return getDetailTranByID(' + item.TransactionID + ')"> Chi tiết</button></td>';
-                    html += '</tr>';
-                });
-                $('#tbodyStatusPP').html(html);
-                $('#tbStatusPP').DataTable({
-                    "responsive": true, "lengthChange": true, "autoWidth": false, "paging": true, "searching": true, "ordering": true, "info": true, retrieve: true,
-                    "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-                }).buttons().container().appendTo('#tbStatusPP_wrapper .col-md-6:eq(0)');
-                //pagingSPP(result.total, function () {
-                //    loadDataStatusParking();
-                //}, changePageSizeSPP);
-            },
-            error: function (errormessage) {
-                if (timeFrom == null && timeTo == null) {
-                } else {
-                    alert(errormessage.responseText);
-                }
-            }
-        });
+    var ParkingPlaceID = $('#cbxListParking').val();
+    $.ajax({
+        url: "/ManagePPlace/LoadDataStatusPP",
+        type: "GET",
+        data: { ParkingPlaceID: ParkingPlaceID },
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            var data = result.dataSSP;
+            var html = '';
+            $.each(data, function (key, item) {
+                html += '<tr>';
+                html += '<td>' + item.LicensePlates + '</td>';
+                html += '<td>' + item.TimeIn + '</td>';
 
-    })  }
+                html += '<td>' + item.TypeOfTicket + '</td>';
+                html += '<td>' + item.CardNumber + '</td>';
+                html += '<td>' + item.TypeOfVerhicleTran + '</td>';
+                html += '<td><button class="btn btn-primary" onclick = "return getDetailTranByID(' + item.TransactionID + ')"> Chi tiết</button></td>';
+                html += '</tr>';
+            });
+            $('#tbodyStatusPP').html(html);
+            $('#tbStatusPP').DataTable({
+                "responsive": true, "lengthChange": true, "autoWidth": false, "paging": true, "searching": true, "ordering": true, "info": true, retrieve: true,
+                "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+            }).buttons().container().appendTo('#tbStatusPP_wrapper .col-md-6:eq(0)');
+        },
+        error: function (errormessage) {
+            if (timeFrom == null && timeTo == null) {
+            } else {
+                alert(errormessage.responseText);
+            }
+        }
+    });
+}
+
+function runInterval() {
+    setInterval(function () {
+        loadDataStatusParking();
+        loadTableList();
+    }, 5000);
+}
+
 function getDetailTranByID(TransactionID) {
     $.ajax({
         url: "/ManagePPlace/ParkingSSDetails/" + TransactionID,
@@ -67,12 +63,12 @@ function getDetailTranByID(TransactionID) {
             $('#Idd').val(result.TransactionID);
             $('#LicensePlatesd').val(result.LicensePlates);
             $('#TimeInd').val(result.TimeIn);
-            
+
             $('#TypeOfTicketd').val(result.typeTicket);
             $('#CardNumber').val(result.CardNumber);
             $('#TypeOfVe').val(result.typeVE);
 
-          
+
 
             $('#myModalTranDetail').modal('show');
         },
