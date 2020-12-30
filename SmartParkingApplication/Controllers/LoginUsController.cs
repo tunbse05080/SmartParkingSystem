35781c796ca367bbd05f1ea44210b299355ee7d4
DuongXuanThang <71684568+DuongXuanThang@users.noreply.github.com>
2020-12-30 11:23:41 +0700

    updateFogot

--- SmartParkingApplication/Controllers/LoginUsController.cs
+++ SmartParkingApplication/Controllers/LoginUsController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.Security;
 
@@ -61,6 +62,20 @@ namespace SmartParkingApplication.Controllers
             return View();
 
         }
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Forgot(string emailUser)
+        {
 
+            string subject = "Yêu cầu đổi mật khẩu";
+            string body = "Mật khẩu mới là: Aa@1234";
+
+            WebMail.Send(emailUser, subject, body, null, null, null, true, null, null, null, null, null, null);
+            ViewBag.mes = "email sent success";
+            ViewBag.mes = "Gửi mail thành công.Bạn vào kiểm tra mật khẩu tại gmail";
+
+            return View();
+
+        }
     }
 }
